77cff173fc5b5449501c277d4b91921aeacb35db
Luke Stoltenberg <lstoltenberg@itbit.com>
2025-02-20 07:49:45 -0600

    weak attribute flag

--- crypto/secp256k1/libsecp256k1/include/secp256k1_recovery.h
+++ crypto/secp256k1/libsecp256k1/include/secp256k1_recovery.h
@@ -38,7 +38,7 @@ SECP256K1_API int secp256k1_ecdsa_recoverable_signature_parse_compact(
     secp256k1_ecdsa_recoverable_signature* sig,
     const unsigned char *input64,
     int recid
-) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3);
+) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) __attribute__((weak));
 
 /** Convert a recoverable signature into a normal signature.
  *
@@ -50,7 +50,7 @@ SECP256K1_API int secp256k1_ecdsa_recoverable_signature_convert(
     const secp256k1_context* ctx,
     secp256k1_ecdsa_signature* sig,
     const secp256k1_ecdsa_recoverable_signature* sigin
-) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3);
+) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) __attribute__((weak));
 
 /** Serialize an ECDSA signature in compact format (64 bytes + recovery id).
  *
